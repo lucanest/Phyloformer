5d1b97e7ee151f2f5d6c90d98af6c7127a5297cf
luca.nesterenko <luca.nesterenko@etu.u-paris.fr>
2022-11-03 18:32:52 +0100

    test script extension fix

--- testdata/test.py
+++ testdata/test.py
@@ -6,8 +6,8 @@ from ete3 import Tree
 scriptdir=os.path.dirname(os.path.realpath(__file__))
 trues=os.path.join(scriptdir,'trees')
 preds=os.path.join(scriptdir,'alignments')
-true_trees=[item for item in os.listdir(trues) if item[-4:]=='tree']
-predicted_trees=[item for item in os.listdir(preds) if item[-4:]=='tree']
+true_trees=[item for item in os.listdir(trues) if item[-3:]=='nwk']
+predicted_trees=[item for item in os.listdir(preds) if item[-3:]=='nwk']
 RFs=[]
 for tree in true_trees:
     t1=Tree(os.path.join(trues,tree))
