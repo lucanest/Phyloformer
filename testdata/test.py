d46e1a97f063532806d48c2c1645eb32df8981dd
Luca Nesterenko <100140639+lucanest@users.noreply.github.com>
2023-03-08 14:35:12 +0100

    test script updated for new outputs extension

--- testdata/test.py
+++ testdata/test.py
@@ -11,6 +11,6 @@ predicted_trees=[item for item in os.listdir(preds) if item[-3:]=='nwk']
 RFs=[]
 for tree in true_trees:
     t1=Tree(os.path.join(trues,tree))
-    t2=Tree(os.path.join(preds,'predicted_'+tree))
+    t2=Tree(os.path.join(preds,tree.split('.nwk')[0]+'.pf.nwk'))
     RFs.append(t1.compare(t2,unrooted=True)['norm_rf'])
 print(f'Mean normalized Robinson-Foulds distance between true and predicted trees: {np.mean(RFs):.3f}')
