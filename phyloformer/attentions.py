Fixing final mean with padding/masking + clean up

phyloformer/attentions.py
@@ -4,8 +4,6 @@ in the Phyloformer network
 
 import torch
 import torch.nn as nn
-import math
-from torch.nn import functional as F
 
 
 class KernelAxialMultiAttention(nn.Module):
@@ -24,7 +22,7 @@ class KernelAxialMultiAttention(nn.Module):
         self.att_drop = nn.Dropout(dropout)
         self.proj_drop = nn.Dropout(dropout)
 
-    def forward2(self, x, mask=None):
+    def einsum_forward(self, x, mask=None):
         # Get dimensions
         B = x.shape[0]
         M, T, C = x.shape[-3:]
@@ -56,13 +54,9 @@ class KernelAxialMultiAttention(nn.Module):
         # reshape values to concatenate along attention heads
         V = V.contiguous().view(B, -1, T, N * D)
 
-        # V = torch.einsum("nlhd,nhmd,nlhm", Q, KV, Z)
-
         return V.contiguous()
 
     def forward(self, x, mask=None):
-        print("\t\tIn Attention:")
-        print(f"\t\t Input: {x.shape}")
         Bs = x.shape[0]
 
         # M:n_pairs, T:seq_len, C:h_dim, N:n_heads, D:h_dim/n_heads (For row attn)
@@ -70,41 +64,27 @@ class KernelAxialMultiAttention(nn.Module):
         M, T, C = x.shape[-3:]
         N, D = self.n_heads, C // self.n_heads
 
-        print(f"\t\tB:{Bs}, M:{M}, T:{T}, C:{C}, N:{N}, D:{D}")
-
         q = self.q_net(x).view(Bs, M, T, N, D).transpose(2, 3)
         k = self.k_net(x).view(Bs, M, T, N, D).transpose(2, 3)
         v = self.v_net(x).view(Bs, M, T, N, D).transpose(2, 3)
-        print(f"\t\tq: {q.shape}, k: {k.shape}, v: {v.shape}")
 
         q = self.elu(q) + 1
         k = self.elu(k) + 1
-        print(f"\t\tQ:{q.shape}, K: {k.shape}")
 
         # Transformers are RNNs linear attention paper adds the mask to the
-        # K matrix in their implementation:
+        # K matrix in their implementation:i
         # https://github.com/idiap/fast-transformers/blob/2ad36b97e64cb93862937bd21fcc9568d989561f/fast_transformers/attention/linear_attention.py#L67
         # mask: (Bs * M * T)
         if mask is not None:
-            print(f"\t\t mask: {mask.shape}")
             mask = mask[:, :, None, :, None]
-            print(f"\t\t mask..: {mask.shape}")
-            print(f"\t\t k:      {k.shape}")
 
             k = k * mask
 
-        print(f"\t\tKt: {k.transpose(-1, -2).shape}")
         KtV = k.transpose(-1, -2) @ v
-        print(f"\t\tKtV: {KtV.shape}")
-        print(f"\t\tKt.sum: {k.transpose(-1, -2).sum(dim=1, keepdim=True).shape}")
         Z = 1 / (q @ k.transpose(-1, -2).sum(dim=-1, keepdim=True) + self.eps)
-        print(f"\t\tZ: {Z.shape}")
         Z = Z.expand(Bs, M, N, T, D)
-        print(f"\t\tZ..: {Z.shape}")
-        V = Z @ KtV
-        print(f"\t\tV: {V.shape}")
+        V = Z @ KtV  # Potentially missing a Q term here ?
         V = V.transpose(2, 3).contiguous().view(Bs, -1, T, N * D)
-        print(f"\t\tV..: {V.shape}")
 
         a = None
         out = self.proj_drop(self.proj_net(V))

phyloformer/phyloformer.py
@@ -183,27 +183,21 @@ class AttentionNet(nn.Module):
             If the tensors aren't the right shape
         """
 
-        # Lets assume that the mask has dimension (batch_size * seq_len * n_seqs)
-
-        print(f"Input TENSOR: {x.shape}")
         attentionmaps = []
         # 2D convolution that gives us the features in the third dimension
         # (i.e. initial embedding of each amino acid)
 
         out = self.block_1_1(x)
-        print(f"First Block: {out.shape}")
 
         out = torch.matmul(self.seq2pair, out.transpose(-1, -2))  # pair representation
-        print(f"Pair representation: {out.shape}")
 
         pair_mask = None
+        # Lets assume that the mask has dimension (batch_size * seq_len * n_seqs)
         if mask is not None:
-            print(f"Mask: {mask.shape}")
             # pair_mask of shape (batch_size * n_pairs * seq_len)
             pair_mask = (
                 torch.matmul(self.seq2pair, mask.transpose(-1, -2)) == 2
             ).float()
-            print(f"Pair_mask: {pair_mask.shape}")
 
         # From here on the tensor has shape (batch_size,features,nb_pairs,seq_len), all
         # the transpose/permute allow to apply layernorm and attention over the desired
@@ -211,59 +205,51 @@ class AttentionNet(nn.Module):
         # of dimensions
 
         out = self.norm(out.transpose(-1, -3)).transpose(-1, -3)  # layernorm
-        print(f"First LayerNorm: {out.shape}")
 
         for i in range(self.n_blocks):
-            print()
             # AXIAL ATTENTIONS BLOCK
             # ----------------------
             # ROW ATTENTION
             att, a = self.rowAttentions[i](out.permute(0, 2, 3, 1), mask=pair_mask)
-            print(f"\tRow attention {(i)}: {att.shape}")
             out = att.permute(0, 3, 1, 2) + out  # row attention+residual connection
-            print(f"\t+Residual {(i)}: {out.shape}")
             out = self.layernorms[i](out.transpose(-1, -3)).transpose(
                 -1, -3
             )  # layernorm
-            print(f"\t+Layer Norm {(i)}: {out.shape}")
 
             # COLUMN ATTENTION
             att, a = self.columnAttentions[i](
                 out.permute(0, 3, 2, 1),
                 mask=(pair_mask.transpose(-1, -2) if pair_mask is not None else None),
             )
-            print(f"\tCol attention {(i)}: {att.shape}")
 
             attentionmaps.append(a)
             out = att.permute(0, 3, 2, 1) + out  # column attention+residual connection
-            # print(f"\t+Residual {(i)}: {out.shape}")
             out = self.layernorms[i](out.transpose(-1, -3)).transpose(
                 -1, -3
             )  # layernorm
-            print(f"\t+Layer Norm {(i)}: {out.shape}")
 
             # FEEDFORWARD
             out = self.fNNs[i](out) + out
-            print(f"\t+FNN {(i)}: {out.shape}")
             if i != self.n_blocks - 1:
                 out = self.layernorms[i](out.transpose(-1, -3)).transpose(
                     -1, -3
                 )  # layernorm
-                print(f"\t+Final Layer Norm {i}: {out.shape}")
 
-        print()
         # After this last convolution we have (batch_size,1,nb_pairs,seq_len)
         out = self.pwFNN(out)
-        print(f"PwFNN: {out.shape}")
+
         # Averaging over positions and removing the extra dimensions
         # we finally get (batch_size,nb_pairs)
-        out = torch.squeeze(torch.mean(out, dim=-1))
-        print(f"Final shape: {out.shape}")
+        if pair_mask is not None:
+            out = (
+                torch.sum(out * pair_mask[:, None, :, :], dim=-1)
+                / torch.sum(pair_mask, dim=-1)[:, None, :]
+            )
+            out = torch.squeeze(out)
+        else:
+            out = torch.squeeze(torch.mean(out, dim=-1))
 
-        # return out, attentionmaps
-        del attentionmaps
-
-        return out
+        return out, attentionmaps
 
     def _get_architecture(self) -> Dict[str, Any]:
         """Returns architecture parameters of the model
@@ -333,8 +319,8 @@ class AttentionNet(nn.Module):
 
         # Infer distances
         with torch.no_grad():
-            predictions = self(tensor.float(), mask=mask)
-        predictions = predictions.view(self.n_pairs)
+            predictions, _ = self(tensor.float(), mask=mask)
+        predictions, _ = predictions.view(self.n_pairs)
 
         if mask is not None:
             pair_mask = (torch.matmul(self.seq2pair, mask.transpose(-1, -2)) == 2).sum(
@@ -342,11 +328,17 @@ class AttentionNet(nn.Module):
             ) != 0
             predictions = predictions[pair_mask[-1]]
 
+        n_seqs = int(
+            self.n_seqs if mask is None else torch.squeeze(mask.sum(-1))[0].int().item()
+        )
+
+        print(f"predicting with {n_seqs} from {self.n_seqs}")
+
         # Build distance matrix
         nn_dist = {}
         cursor = 0
-        for i in range(self.n_seqs):
-            for j in range(self.n_seqs):
+        for i in range(n_seqs):
+            for j in range(n_seqs):
                 if i == j:
                     nn_dist[(i, j)] = 0
                 if i < j:
@@ -356,7 +348,7 @@ class AttentionNet(nn.Module):
                     cursor += 1
 
         return skbio.DistanceMatrix(
-            [[nn_dist[(i, j)] for j in range(self.n_seqs)] for i in range(self.n_seqs)],
+            [[nn_dist[(i, j)] for j in range(n_seqs)] for i in range(n_seqs)],
             ids=ids,
         )
 