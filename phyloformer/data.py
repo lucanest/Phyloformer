969e612a03130c1db5bdfff7027ebc6687827fdc
Luc Blassel <luc.blassel@gmail.com>
2024-06-24 19:15:22 +0200

    Cleaning up training script

--- phyloformer/data.py
+++ phyloformer/data.py
@@ -1,4 +1,8 @@
+from itertools import combinations
+
+import dendropy
 import torch
+from torch.utils.data import Dataset
 
 ALPHABET = b"ARNDCQEGHILKMFPSTWYVX-"
 LOOKUP = {char: index for index, char in enumerate(ALPHABET)}
@@ -25,3 +29,47 @@ def load_alignment(filepath):
     seqs = torch.nn.functional.one_hot(seqs, num_classes=len(ALPHABET)).permute(2, 1, 0)
 
     return seqs, ids
+
+
+def load_distance_matrix(filepath, ids):
+    """
+    Reads a newick formatted tree and returns a vector of the
+    upper triangle of the corresponding pairwise distance matrix.
+    The order of taxa in the rows and columns of the corresponding
+    distance matrix is given by the `ids` input list.
+    """
+
+    distances = []
+
+    with open(filepath, "r") as treefile:
+        tree = dendropy.Tree.get(file=treefile, schema="newick")
+    taxa = tree.taxon_namespace
+    dm = tree.phylogenetic_distance_matrix()
+    for tip1, tip2 in combinations(ids, 2):
+        l1, l2 = taxa.get_taxon(tip1), taxa.get_taxon(tip2)
+        distances.append(dm.distance(l1, l2))
+
+    return torch.tensor(distances)
+
+
+class PhyloDataset(Dataset):
+    """
+    Simple pytorch dataset that reads tree/alignment pairs
+    and returns the corresponding tensor objects
+    """
+
+    def __init__(self, pairs):
+        """
+        pairs: List[(str,str)] = a list of (treefile, alnfile) paths
+        """
+        self.pairs = pairs
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, index):
+        treefile, alnfile = self.pairs[index]
+        x, ids = load_alignment(alnfile)
+        y = load_distance_matrix(treefile, ids)
+
+        return x, y
